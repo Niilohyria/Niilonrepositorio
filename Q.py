6316716071503d75c73e29dff2cf18ab41686fe1
kayttaja <etunimi.sukunimi@edu.hyria.fi>
2025-09-01 11:03:49 +0300

    Lisäsin koodia git push git status

---
+++ Q.py
@@ -0,0 +1,5 @@
+Question = input("What do you know about Hearts Of Iron IV?")
+if Question == "Nothing":
+    print ("You're full of shit.")
+if Question == "Everything":
+    print ("You're amazing.")
