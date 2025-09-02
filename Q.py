7c699a67b6a79449f9919a0ae818420182c7a0fc
Niilohyria <niilo.mustonen@edu.hyria.fi>
2025-09-02 09:12:32 +0300

    Update Q.py
    Qwerty

--- Q.py
+++ Q.py
@@ -5,3 +5,4 @@ if Question == "Everything":
     print ("You're amazing.")
 if Question == "qwertyuiopåasdfghjklöäzxcvbnm":
     print ("Get help")
+print ("Did you say the right thing, you foolish mortal?")
