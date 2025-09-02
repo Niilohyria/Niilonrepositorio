eb3e19ed176c01458065358090ff7725577400de
Niilohyria <niilo.mustonen@edu.hyria.fi>
2025-09-02 08:58:00 +0300

    Update Q.py

--- Q.py
+++ Q.py
@@ -3,3 +3,5 @@ if Question == "Nothing":
     print ("You're full of shit.")
 if Question == "Everything":
     print ("You're amazing.")
+if Question == "qwertyuiopåasdfghjklöäzxcvbnm":
+    print ("Get help")
